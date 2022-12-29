445e2c67e07a1dab5d570dde32ed1de513402beb
Bola Ghaly <bolaghaly63@gmail.com>
2022-12-29 01:45:53 -0500

    Create new line chart

---
+++ pages/LineChart.tsx
@@ -0,0 +1,54 @@
+import { ResponsiveLine } from "@nivo/line";
+
+const LineChart = ({ data }) => (
+  <ResponsiveLine
+    data={data}
+    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+    xScale={{ type: "point" }}
+    yScale={{
+      type: "linear",
+      min: "0",
+      max: "20000",
+      stacked: false,
+      reverse: false,
+    }}
+    yFormat=" >-.2f"
+    curve="natural"
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      orient: "bottom",
+      tickSize: 0,
+      tickPadding: 8,
+      tickRotation: 0,
+    }}
+    axisLeft={{
+      orient: "left",
+      tickSize: 0,
+      tickPadding: 8,
+      tickRotation: 0,
+    }}
+    pointColor={{ theme: "labels.text.fill" }}
+    pointBorderColor={{ from: "serieColor" }}
+    pointLabelYOffset={-12}
+    useMesh={true}
+    legends={[
+      {
+        anchor: "top-right",
+        direction: "row",
+        justify: false,
+        translateX: 6,
+        translateY: -30,
+        itemsSpacing: 0,
+        itemDirection: "left-to-right",
+        itemWidth: 100,
+        itemHeight: 20,
+        symbolSize: 12,
+        symbolShape: "circle",
+        symbolBorderColor: "rgba(0, 0, 0, .5)",
+      },
+    ]}
+  />
+);
+
+export default LineChart;
