073fb5acaf7d3ac068796717d9993e234d7395e2
Bola Ghaly <polaghaly63@gmail.com>
2023-01-04 00:43:35 -0500

    Update layout and sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,21 @@
 import Sidebar from "./Sidebar";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 const Layout = ({ children }) => {
+  const darkMode = useSelector((state: RootState) => state.darkMode.value);
+  const sideBar = useSelector((state: RootState) => state.openSideBar.value);
+
   return (
     <div className="font-poppins">
       <Sidebar />
-      <main>{children}</main>
+      <main
+        className={`${sideBar ? "cursor-pointer " : ""} ${
+          darkMode ? "dark" : ""
+        }`}
+      >
+        {children}
+      </main>
     </div>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,19 @@
-import { useState, useEffect } from "react";
 import { BiArrowToRight, BiArrowToLeft } from "react-icons/bi";
 import { RiDashboardLine } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
 import useOnclickOutside from "react-cool-onclickoutside";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { toggledarkMode } from "../store/darkMode";
+import { openSideBarOff, openSideBarReverse } from "../store/sideBar";
 
 const Sidebar = () => {
-  const [openSideBar, setOpenSideBar] = useState(false);
-
-  const toggleOpen = () => {
-    setOpenSideBar(!openSideBar);
-  };
-
-  const ref = useOnclickOutside(() => setOpenSideBar(false));
+  const darkMode = useSelector((state: RootState) => state.darkMode.value);
+  const openSideBar = useSelector((state: RootState) => state.openSideBar.value);
+  const dispatch = useDispatch();
+  const ref = useOnclickOutside(() => dispatch(openSideBarOff()));
 
   const navData = [
     {
@@ -31,16 +32,23 @@ const Sidebar = () => {
 
   return (
     <nav
-      className={`sideNav fixed top-0 left-0 h-screen bg-neutral-500 pt-7 ${
+      className={`sideNav absolute top-0 left-0 h-screen bg-neutral-500 pt-7 ${
         openSideBar ? "w-[200px]" : "w-[60px]"
       }`}
       ref={ref}
     >
       <button
         className="cursor-pointer pl-5 border-none bg-transparent justify-self-end"
-        onClick={toggleOpen}
+        onClick={() => dispatch(openSideBarReverse())}
       >
         {openSideBar ? <BiArrowToLeft /> : <BiArrowToRight />}
+      </button>
+
+      <button className="px-5 ">
+        <BsFillMoonStarsFill
+          className="text-xl"
+          onClick={() => dispatch(toggledarkMode())}
+        />
       </button>
       {navData.map((item) => {
         return (
@@ -49,8 +57,7 @@ const Sidebar = () => {
             className="flex items-center py-[10px] px-5"
             href={item.link}
           >
-            <div className="text-xl">{item.icon}   </div>
-                 
+            <div className="text-xl">{item.icon}</div>
             <span className={`${openSideBar ? "" : "invisible"}`}>
               {item.text}
             </span>
