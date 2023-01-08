f2b70e9d8b981d706316d9d4b7924c9aab971c0e
Bola Ghaly <bolaghaly63@gmail.com>
2023-01-07 23:50:05 -0500

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,11 +6,22 @@ const Layout = ({ children }) => {
   const openSideBar = useSelector(
     (state: RootState) => state.openSideBar.value
   );
+  const darkMode = useSelector((state: RootState) => state.darkMode.value);
 
   return (
-    <main className="font-poppins">
+    <main
+      className={`font-poppins ${openSideBar ? "cursor-pointer" : ""} ${
+        darkMode ? "text-white" : "text-black"
+      }`}
+    >
       <Sidebar />
-      <div className={`${openSideBar ? "cursor-pointer" : ""}`}>{children}</div>
+      <section
+        className={`ml-[60px] p-4 min-h-screen transition-colors duration-500 ${
+          darkMode ? "bg-neutral-900" : "bg-neutral-50"
+        } `}
+      >
+        {children}
+      </section>
     </main>
   );
 };
