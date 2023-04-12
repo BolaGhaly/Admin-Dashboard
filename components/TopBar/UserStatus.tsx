17da6febf32c197c4f396c19cafa2db97bfc166d
BolaGhaly <bolaghaly63@gmail.com>
2023-04-11 20:09:53 -0400

    Add framer motion to user status dropdown menu

--- components/TopBar/UserStatus.tsx
+++ components/TopBar/UserStatus.tsx
@@ -1,20 +1,46 @@
-import { useState, useRef } from "react";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Grow from "@mui/material/Grow";
-import Popper from "@mui/material/Popper";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
+import { useState } from "react";
 import styles from "./topBar.module.scss";
 import Icon from "../../utils/topBarUtils";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { online, offline, away } from "../../store/userStatus";
+import useOnclickOutside from "react-cool-onclickoutside";
+import { motion, Variants } from "framer-motion";
 
 const UserStatus = () => {
-  const anchorRef = useRef(null);
   const [userStatusOpen, setUserStatusOpen] = useState(false);
   const userStatus = useSelector((state: RootState) => state.userStatus.value);
   const dispatch = useDispatch();
+  const userStatusMenuRef = useOnclickOutside(() => {
+    handleClose();
+  });
+
+  const userStatusMenu: Variants = {
+    open: {
+      opacity: 1,
+      display: "block",
+      transition: {
+        ease: "easeOut",
+        duration: 0.2,
+      },
+      originX: -5,
+      originY: -5,
+      translateX: 0,
+      translateY: 0,
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 0.2,
+      },
+      translateX: -5,
+      translateY: -5,
+      transitionEnd: {
+        display: "none",
+      },
+    },
+  };
 
   const handleToggle = () => {
     setUserStatusOpen(!userStatusOpen);
@@ -25,14 +51,20 @@ const UserStatus = () => {
   };
 
   return (
-    <div className={styles.userStatus}>
+    <div className={styles.userStatus} ref={userStatusMenuRef}>
       <button
-        ref={anchorRef}
-        id="composition-button"
-        aria-controls={userStatusOpen ? "composition-menu" : undefined}
         aria-expanded={userStatusOpen ? "true" : undefined}
         aria-haspopup="true"
         onClick={handleToggle}
+        title={
+          userStatus === "online"
+            ? "Online"
+            : userStatus === "away"
+            ? "Away"
+            : userStatus === "offline"
+            ? "Offline"
+            : ""
+        }
       >
         {userStatus === "online" ? (
           <>
@@ -53,64 +85,49 @@ const UserStatus = () => {
         <Icon
           name="arrowDown"
           className={`${styles.userStatusArrow} ${
-            userStatusOpen ? `${styles.active}` : null
+            userStatusOpen ? `${styles.arrowActive}` : null
           }`}
         />
       </button>
-      <Popper
-        open={userStatusOpen}
-        anchorEl={anchorRef.current}
-        placement="bottom-start"
-        transition
-        disablePortal
+      <motion.div
+        variants={userStatusMenu}
+        initial={false}
+        animate={userStatusOpen ? "open" : "closed"}
+        className={styles.userStatusMenu}
       >
-        {({ TransitionProps }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin: "left top",
+        <motion.ul>
+          <motion.li
+            onClick={() => {
+              dispatch(online());
+              handleClose();
             }}
+            title="Online"
           >
-            <div className={styles.userStatusMenu}>
-              <ClickAwayListener onClickAway={() => handleClose()}>
-                <MenuList
-                  autoFocusItem={userStatusOpen}
-                  id="composition-menu"
-                  aria-labelledby="composition-button"
-                >
-                  <MenuItem
-                    onClick={() => {
-                      dispatch(online());
-                      handleClose();
-                    }}
-                  >
-                    <span className={styles.userStatusOnline} />
-                    <p>Online</p>
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => {
-                      dispatch(away());
-                      handleClose();
-                    }}
-                  >
-                    <span className={styles.userStatusAway} />
-                    <p>Away</p>
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => {
-                      dispatch(offline());
-                      handleClose();
-                    }}
-                  >
-                    <span className={styles.userStatusOffline} />
-                    <p>Offline</p>
-                  </MenuItem>
-                </MenuList>
-              </ClickAwayListener>
-            </div>
-          </Grow>
-        )}
-      </Popper>
+            <span className={styles.userStatusOnline} />
+            <p>Online</p>
+          </motion.li>
+          <motion.li
+            onClick={() => {
+              dispatch(away());
+              handleClose();
+            }}
+            title="Away"
+          >
+            <span className={styles.userStatusAway} />
+            <p>Away</p>
+          </motion.li>
+          <motion.li
+            onClick={() => {
+              dispatch(offline());
+              handleClose();
+            }}
+            title="Offline"
+          >
+            <span className={styles.userStatusOffline} />
+            <p>Offline</p>
+          </motion.li>
+        </motion.ul>
+      </motion.div>
     </div>
   );
 };
