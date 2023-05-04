9f3b2ee16d90a581c691a69778fe5cd0bd2a39d0
BolaGhaly <bolaghaly63@gmail.com>
2023-05-04 00:40:28 -0400

    Add new google font for company name

--- fonts.ts
+++ fonts.ts
@@ -1,5 +1,10 @@
-import { Poppins } from "@next/font/google";
+import { Poppins, Lobster } from "@next/font/google";
 export const poppinsFont = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600"],
+});
+
+export const lobsterFont = Lobster({
+  subsets: ["latin"],
+  weight: "400"
 });
