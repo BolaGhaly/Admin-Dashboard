521371b9ade65fc1692d605de2af872535fbcf5e
BolaGhaly <bolaghaly63@gmail.com>
2023-05-11 02:45:17 -0400

    Add .active class based on button clicked (first by default)

--- components/SideBar/SideBarMenu.tsx
+++ components/SideBar/SideBarMenu.tsx
@@ -1,23 +1,37 @@
 import Icon, { sideBarMenuData } from "../../utils/sideBarUtils";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { closeSideBar } from "../../store/slices/sideBar";
 import { RootState } from "../../store/store";
 import styles from "./sideBar.module.scss";
 import Link from "next/link";
+import { useState } from "react";
 
 const SideBarMenu = () => {
   const openSideBar = useSelector(
     (state: RootState) => state.openSideBar.value
   );
+  const [activeItem, setActiveItem] = useState(sideBarMenuData[0]);
+  const dispatch = useDispatch();
+
+  const handleClick = (item: string) => {
+    setActiveItem(item);
+    dispatch(closeSideBar());
+  };
 
   return (
     <div className={styles.sideBarMenu}>
       {sideBarMenuData.map((item, idx) => {
         return (
-          <Link href="#" key={idx}>
+          <Link
+            href="#"
+            key={idx}
+            onClick={() => handleClick(item)}
+            className={`${activeItem === item ? styles.active : null}`}
+          >
             <button>
               <Icon name={`${item}`} />
             </button>
-            <span className={`${openSideBar ? "" : "hidden"}`}>
+            <span className={`${openSideBar ? null : "hidden"}`}>
               {item.charAt(0).toUpperCase() + item.slice(1)}
             </span>
           </Link>
