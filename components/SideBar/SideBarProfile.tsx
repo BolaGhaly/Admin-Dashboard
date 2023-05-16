f21b6bbded9649c374c17c1bb2e460545c86947e
BolaGhaly <bolaghaly63@gmail.com>
2023-05-16 00:23:40 -0400

    Update sidebar profile component

--- components/SideBar/SideBarProfile.tsx
+++ components/SideBar/SideBarProfile.tsx
@@ -1,23 +1,29 @@
 import Icon from "../../utils/sideBarUtils";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store/store";
-//import styles from "../styles/modules/sideBar.module.scss";
+import styles from "./sideBar.module.scss";
+import { closeSideBar } from "../../store/slices/sideBar";
 
 const SideBarProfile = () => {
   const openSideBar = useSelector(
     (state: RootState) => state.openSideBar.value
   );
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <a href="#">
-        <Icon name="profileSettings" />
-        <p className={`${openSideBar ? null : "hidden"}`}> Support</p>
-      </a>
-      <a href="#">
-        <Icon name="signOut" />
-        <p className={`${openSideBar ? null : "hidden"}`}> Sign Out</p>
-      </a>
+    <div className={styles.sideBarProfile}>
+      <div onClick={() => dispatch(closeSideBar())}>
+        <button>
+          <Icon name="profileSettings" />
+        </button>
+        <span className={`${openSideBar ? null : "hidden"}`}>Support</span>
+      </div>
+      <div onClick={() => dispatch(closeSideBar())}>
+        <button>
+          <Icon name="signOut" />
+        </button>
+        <span className={`${openSideBar ? null : "hidden"}`}>Sign Out</span>
+      </div>
     </div>
   );
 };
