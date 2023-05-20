8d7483bff425593fc75136e68b9b610ef60f439b
BolaGhaly <bolaghaly63@gmail.com>
2023-05-20 18:21:43 -0400

    Add a staggering effect to side bar menu span tags

--- components/SideBar/SideBarMenu.tsx
+++ components/SideBar/SideBarMenu.tsx
@@ -5,6 +5,7 @@ import { RootState } from "../../store/store";
 import styles from "./sideBar.module.scss";
 import Link from "next/link";
 import { useState } from "react";
+import { motion, Variants } from "framer-motion";
 
 const SideBarMenu = () => {
   const openSideBar = useSelector(
@@ -18,26 +19,57 @@ const SideBarMenu = () => {
     dispatch(closeSideBar());
   };
 
+  const itemVariants: Variants = {
+    open: (index: number) => ({
+      display: "block",
+      opacity: 1,
+      visibility: "visible",
+      transition: {
+        duration: 0.1,
+        delay: index,
+        ease: "easeOut",
+      },
+    }),
+    closed: {
+      display: "none",
+      opacity: 0,
+      visibility: "hidden",
+    },
+  };
+
   return (
     <div className={styles.sideBarMenu}>
-      {sideBarMenu.map((item, idx) => {
-        return (
-          <Link
-            title={item.charAt(0).toUpperCase() + item.slice(1)}
-            href="#"
-            key={idx}
-            onClick={() => handleClick(item)}
-            className={`${activeItem === item ? styles.active : null}`}
+      {sideBarMenu.map((item, idx) => (
+        <Link
+          title={item.charAt(0).toUpperCase() + item.slice(1)}
+          href="#"
+          key={idx}
+          onClick={() => handleClick(item)}
+          className={`${activeItem === item ? styles.active : null}`}
+        >
+          <button title={item.charAt(0).toUpperCase() + item.slice(1)}>
+            <Icon name={`${item}`} />
+          </button>
+          <motion.span
+            initial={false}
+            variants={itemVariants}
+            animate={openSideBar ? "open" : "closed"}
+            custom={
+              idx === 0
+                ? 0.1
+                : idx === 1
+                ? 0.2
+                : idx === 2
+                ? 0.3
+                : idx === 3
+                ? 0.4
+                : null
+            }
           >
-            <button title={item.charAt(0).toUpperCase() + item.slice(1)}>
-              <Icon name={`${item}`} />
-            </button>
-            <span className={`${openSideBar ? null : "hidden"}`}>
-              {item.charAt(0).toUpperCase() + item.slice(1)}
-            </span>
-          </Link>
-        );
-      })}
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </motion.span>
+        </Link>
+      ))}
     </div>
   );
 };
