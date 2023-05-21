adcbe34f45010cc68eefe8b722a3fea636586a2b
BolaGhaly <bolaghaly63@gmail.com>
2023-05-20 21:14:16 -0400

    Slowly fade in side bar profile span tags

--- components/SideBar/SideBarProfile.tsx
+++ components/SideBar/SideBarProfile.tsx
@@ -3,12 +3,29 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store/store";
 import styles from "./sideBar.module.scss";
 import { closeSideBar } from "../../store/slices/sideBar";
+import { motion, Variants } from "framer-motion";
 
 const SideBarProfile = () => {
   const openSideBar = useSelector(
     (state: RootState) => state.openSideBar.value
   );
   const dispatch = useDispatch();
+  const itemVariants: Variants = {
+    open: {
+      display: "block",
+      opacity: 1,
+      visibility: "visible",
+      transition: {
+        duration: 0.2,
+        ease: "easeOut",
+      },
+    },
+    closed: {
+      display: "none",
+      opacity: 0,
+      visibility: "hidden",
+    },
+  };
 
   return (
     <div className={styles.sideBarProfile}>
@@ -21,9 +38,13 @@ const SideBarProfile = () => {
           <button title={item.title}>
             <Icon name={item.icon} />
           </button>
-          <span className={`${openSideBar ? null : "hidden"}`}>
+          <motion.span
+            initial={false}
+            variants={itemVariants}
+            animate={openSideBar ? "open" : "closed"}
+          >
             {item.title}
-          </span>
+          </motion.span>
         </div>
       ))}
     </div>
