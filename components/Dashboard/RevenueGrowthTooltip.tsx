2229982dc196774db01da31a0891b80012afdf11
BolaGhaly <bolaghaly63@gmail.com>
2023-06-07 09:01:15 -0400

    Add custom revenue growth tooltip component and mainUtils.tsx file

---
+++ components/Dashboard/RevenueGrowthTooltip.tsx
@@ -0,0 +1,39 @@
+import { TooltipProps } from "recharts";
+import {
+  ValueType,
+  NameType,
+} from "recharts/types/component/DefaultTooltipContent";
+import styles from "./dashboard.module.scss";
+import { numFormatter } from "../../utils/mainUtils";
+
+const RevenueGrowthTooltip = ({
+  active,
+  payload,
+  label,
+}: TooltipProps<ValueType, NameType>) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className={styles.customTooltip}>
+        <p>{`${label}`}</p>
+        <div>
+          {payload.map((item, idx) => (
+            <div key={idx}>
+              <span
+                style={{
+                  backgroundColor: `${item.stroke}`,
+                }}
+              />
+              <p>
+                {item.name}: ${numFormatter(item.value, 2)}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default RevenueGrowthTooltip;
---
+++ utils/mainUtils.ts
@@ -0,0 +1,20 @@
+export const numFormatter = (num, digits) => {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  var item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item
+    ? (num / item.value).toFixed(digits) + item.symbol
+    : "0";
+};
