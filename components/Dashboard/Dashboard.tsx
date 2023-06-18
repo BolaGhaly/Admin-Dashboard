6d290176ba940938ea5fc84630972e5f9dffe9ad
BolaGhaly <bolaghaly63@gmail.com>
2023-06-18 01:05:00 -0400

    Refactor dashboard.tsx

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,35 +1,9 @@
 import Head from "next/head";
 import styles from "./dashboard.module.scss";
 import TotalData from "./TotalData";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
-import { useState, useEffect } from "react";
-import RevenueGrowthTooltip from "./RevenueGrowthTooltip";
-import RevenueGrowthLegend from "./RevenueGrowthLegend";
-import { RevenueGrowthData } from "../../utils/dashboardUtils";
+import RevenueGrowthChart from "./RevenueGrowthChart";
 
 const Dashboard = () => {
-  const [windowWidth, setWindowWidth] = useState<number>(
-    typeof window !== "undefined" ? window.innerWidth : 0
-  );
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
-  };
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
       <Head>
@@ -39,66 +13,7 @@ const Dashboard = () => {
         <h1>Dashboard</h1>
         <TotalData />
         <div className={styles.revenueCharts}>
-          <div className={styles.revenueGrowthChart}>
-            <h2>Revenue Growth</h2>
-            <RevenueGrowthLegend />
-            <ResponsiveContainer>
-              <LineChart
-                data={RevenueGrowthData}
-                margin={{ right: 20, top: 10 }}
-              >
-                <CartesianGrid
-                  strokeDasharray="5 25"
-                  stroke="var(--notifications-line)"
-                />
-                <XAxis
-                  dataKey="day"
-                  fontSize="0.875rem"
-                  interval={windowWidth >= 375 ? 0 : 1}
-                  tick={{ fill: "var(--text)" }}
-                  stroke="var(--text)"
-                  tickMargin={5}
-                />
-                <YAxis
-                  fontSize="0.875rem"
-                  tick={{ fill: "var(--text)" }}
-                  stroke="var(--text)"
-                  tickMargin={5}
-                />
-                <Tooltip
-                  content={<RevenueGrowthTooltip />}
-                  isAnimationActive={false}
-                  cursor={{ stroke: "var(--text)", strokeDasharray: "6 4" }}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="This Week"
-                  stroke="var(--revenueGrowthLine1)"
-                  dot={{ r: 0 }}
-                  activeDot={{
-                    r: 6,
-                    strokeWidth: 2,
-                    stroke: "var(--dashboard-bg)",
-                  }}
-                  strokeWidth={2}
-                  animationEasing="ease-out"
-                />
-                <Line
-                  type="monotone"
-                  dataKey="Last Week"
-                  stroke="var(--revenueGrowthLine2)"
-                  dot={{ r: 0 }}
-                  activeDot={{
-                    r: 6,
-                    strokeWidth: 2,
-                    stroke: "var(--dashboard-bg)",
-                  }}
-                  strokeWidth={2}
-                  animationEasing="ease-out"
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
+          <RevenueGrowthChart />
         </div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
