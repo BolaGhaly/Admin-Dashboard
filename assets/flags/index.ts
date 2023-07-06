822d1b663365af21d989cf698748f0cd20f906de
BolaGhaly <bolaghaly63@gmail.com>
2023-07-06 00:11:48 -0400

    Update indext.ts to import and export all flags

--- assets/flags/index.ts
+++ assets/flags/index.ts
@@ -4,5 +4,30 @@ import Italy from "./Italy.svg";
 import Germany from "./Germany.svg";
 import Russia from "./Russia.svg";
 import Egypt from "./Egypt.svg";
+import Ecuador from "./Ecuador.svg";
+import Mexico from "./Mexico.svg";
+import Canada from "./Canada.svg";
+import Puerto_Rico from "./Puerto_Rico.svg";
+import UK from "./UK.svg";
+import Nigeria from "./Nigeria.svg";
+import South_Africa from "./South_Africa.svg";
+import Japan from "./Japan.svg";
+import Australia from "./Australia.svg";
 
-export { USA, Spain, Italy, Germany, Russia, Egypt };
+export {
+  USA,
+  Spain,
+  Italy,
+  Germany,
+  Russia,
+  Egypt,
+  Ecuador,
+  Mexico,
+  Canada,
+  Puerto_Rico,
+  UK,
+  Nigeria,
+  South_Africa,
+  Japan,
+  Australia,
+};
