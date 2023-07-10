6c58fb972bf0afe1c9b03aa911d55688a75b8b01
BolaGhaly <bolaghaly63@gmail.com>
2023-07-10 19:31:59 -0400

    Remove some flags

--- assets/flags/index.ts
+++ assets/flags/index.ts
@@ -6,13 +6,10 @@ import Russia from "./Russia.svg";
 import Egypt from "./Egypt.svg";
 import Ecuador from "./Ecuador.svg";
 import Mexico from "./Mexico.svg";
-import Canada from "./Canada.svg";
 import Puerto_Rico from "./Puerto_Rico.svg";
 import UK from "./UK.svg";
-import Nigeria from "./Nigeria.svg";
 import South_Africa from "./South_Africa.svg";
 import Japan from "./Japan.svg";
-import Australia from "./Australia.svg";
 
 export {
   USA,
@@ -23,11 +20,8 @@ export {
   Egypt,
   Ecuador,
   Mexico,
-  Canada,
   Puerto_Rico,
   UK,
-  Nigeria,
   South_Africa,
   Japan,
-  Australia,
 };
