8fb65ab03511c21cd6b7c91b5b19b4459288aa83
BolaGhaly <bolaghaly63@gmail.com>
2023-08-09 14:54:25 -0400

    Add framer motion animation to countriesPercentages.tsx

--- components/Dashboard/CountriesPercentages.tsx
+++ components/Dashboard/CountriesPercentages.tsx
@@ -1,13 +1,22 @@
 import styles from "./dashboard.module.scss";
 import { sortArrOfObjsByKeyValue } from "../../utils/mainUtils";
 import Icon, { countriesPercentages } from "../../utils/dashboardUtils";
+import { motion } from "framer-motion";
 
 const CountriesPercentages = () => {
   return (
     <div className={styles.revenueByLocPercentages}>
       {sortArrOfObjsByKeyValue(countriesPercentages, "percentage", true).map(
         (country, idx) => (
-          <div key={idx}>
+          <motion.div
+            key={idx}
+            initial={{ opacity: 0, visibility: "hidden" }}
+            animate={{ opacity: 1, visibility: "visible" }}
+            transition={{
+              duration: 0.2,
+              ease: "easeOut",
+            }}
+          >
             <Icon name={country.flag} />
             <div>
               <p>{country.country}</p>
@@ -23,7 +32,7 @@ const CountriesPercentages = () => {
               <span style={{ width: `calc(100% - ${country.percentage}%)` }} />
             </div>
             <p>{Number.parseFloat(country.percentage).toFixed(2)}%</p>
-          </div>
+          </motion.div>
         )
       )}
     </div>
