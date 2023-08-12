e6cc825d87a82c4f03e5f14f6f9e3eb3622928ac
BolaGhaly <bolaghaly63@gmail.com>
2023-08-12 18:03:38 -0400

    Update countries percentages divs

--- components/Dashboard/RevenueByLocation/CountriesPercentages.tsx
+++ components/Dashboard/RevenueByLocation/CountriesPercentages.tsx
@@ -18,21 +18,29 @@ const CountriesPercentages = () => {
             }}
             title={country.country}
           >
-            <Icon name={country.flag} />
+            <div>
+              <Icon name={country.flag} />
+            </div>
             <div>
               <p>{country.country}</p>
-              <span
-                style={{
-                  width: `${country.percentage}%`,
-                  borderRadius:
-                    country.percentage < 100
-                      ? "0.25rem 0 0 0.25rem"
-                      : "0.25rem",
-                }}
-              />
-              <span style={{ width: `calc(100% - ${country.percentage}%)` }} />
+              <div>
+                <div>
+                  <span
+                    style={{
+                      width: `${country.percentage}%`,
+                      borderRadius:
+                        country.percentage < 100
+                          ? "0.25rem 0 0 0.25rem"
+                          : "0.25rem",
+                    }}
+                  />
+                  <span
+                    style={{ width: `calc(100% - ${country.percentage}%)` }}
+                  />
+                </div>
+                <p>{Number.parseFloat(country.percentage).toFixed(2)}%</p>
+              </div>
             </div>
-            <p>{Number.parseFloat(country.percentage).toFixed(2)}%</p>
           </motion.div>
         )
       )}
