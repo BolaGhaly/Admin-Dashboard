ecfa99cfc1387bfb7e7e3fe8fda0b5422abae829
BolaGhaly <bolaghaly63@gmail.com>
2023-08-15 03:03:22 -0400

    Add google analytics and google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,12 @@ const Document = () => {
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W3HLQCMV"
+height="0" width="0" style="display:none;visibility:hidden" />`,
+          }}
+        />
       </body>
     </Html>
   );
