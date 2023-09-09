bbcd1e079157e2865554c271c4ca1eb63a07fe9c
BolaGhaly <bolaghaly63@gmail.com>
2023-09-09 00:26:25 -0400

    Update top navbar notifications icon

--- utils/topBarUtils.tsx
+++ utils/topBarUtils.tsx
@@ -2,18 +2,18 @@ import { BiSupport } from "react-icons/bi";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { HiOutlineMenu } from "react-icons/hi";
 import { RiLogoutBoxRLine, RiUserSettingsLine } from "react-icons/ri";
-import { TbBellFilled } from "react-icons/tb";
 import moon from "/assets/topBarIcons/moon.svg";
 import sun from "/assets/topBarIcons/sun.svg";
 import emptyNotificationsLight from "/assets/topBarIcons/emptyNotificationsLight.svg";
 import emptyNotificationsDark from "/assets/topBarIcons/emptyNotificationsDark.svg";
+import notificationsIcon from "/assets/topBarIcons/notificationsIcon.svg";
 import { USA, Spain, Italy, Germany, Russia } from "../assets/flags";
 
 const iconTypes = {
   menu: HiOutlineMenu,
   darkModeOff: moon,
   darkModeOn: sun,
-  notifications: TbBellFilled,
+  notifications: notificationsIcon,
   profileSettings: RiUserSettingsLine,
   support: BiSupport,
   signOut: RiLogoutBoxRLine,
