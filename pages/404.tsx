9900be9efed88531fbd3d7475e818114738b84a5
BolaGhaly <bolaghaly63@gmail.com>
2023-09-09 22:53:08 -0400

    Update 404.tsx and its styling

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
-import { poppinsFont } from "../fonts";
+import {
+  changeActiveItem,
+  initialState,
+} from "../store/slices/sideBarMenuActive";
+import { useDispatch } from "react-redux";
 
 const Custom404 = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className="custom404">
-        <h1>404</h1>
-        <span />
-        <p>This page could not be found.</p>
-        <button className={poppinsFont.className}>
-          <Link href="/">Go Back</Link>
-        </button>
+      <h1>404</h1>
+      <span />
+      <p>This page could not be found.</p>
+      <Link
+        href="/"
+        onClick={() =>
+          dispatch(changeActiveItem({ activeItem: initialState.activeItem }))
+        }
+      >
+        Go Back
+      </Link>
     </div>
   );
 };
