213607af43774e5b781e079ee45411671965a4e0
BolaGhaly <bolaghaly63@gmail.com>
2023-09-10 01:52:43 -0400

    Update 404.tsx to change sideBarMenu's active item to URL's endpoint

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,10 +4,18 @@ import {
   initialState,
 } from "../store/slices/sideBarMenuActive";
 import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Custom404 = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
+  const currURLEndpoint = router.asPath.slice(1);
 
+  useEffect(() => {
+    dispatch(changeActiveItem({ activeItem: currURLEndpoint }));
+  }, [currURLEndpoint, dispatch, router]);
+  
   return (
     <div className="custom404">
       <h1>404</h1>
